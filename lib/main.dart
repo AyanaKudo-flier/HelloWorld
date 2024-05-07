a43f523cccafff5b29551fc96acb37bbb5c9841d
Ayana Kudo <ayana.kudo@Kudos-MacBook-Pro.local>
2024-05-07 17:44:38 +0900

    add:急上昇アイテムのセルを追加/不要な部品を削除

--- lib/main.dart
+++ lib/main.dart
@@ -50,29 +50,9 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           children: <Widget>[
-            Container(
-              height: 100,
-              width: 200,
-              color: Colors.blue,
-            ),
-            const Text(
-              'Hello, World4!',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('これはRowです'),
-                SizedBox(width: 20),
-                Text('これはRowです'),
-              ],
-            ),
             const SizedBox(height: 50),
-            const Align(alignment: Alignment.centerLeft, child: Text('ランキング')),
-            // アイテムのセルエリア
+            const Align(alignment: Alignment.centerLeft, child: Text('急上昇')),
+            // 急上昇アイテムのセルエリア
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -94,7 +74,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               height: 90),
                           const Align(
                               alignment: Alignment.bottomRight,
-                              child: Icon(Icons.bookmark, size: 24)),
+                              child: Icon(Icons.bookmark_border_outlined,
+                                  size: 24)),
+                          const Align(
+                              alignment: Alignment.topLeft,
+                              child: Icon(Icons.trending_up, size: 24)),
                         ]),
                         const Align(
                             alignment: Alignment.centerLeft,
@@ -154,7 +138,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               height: 90),
                           const Align(
                               alignment: Alignment.bottomRight,
-                              child: Icon(Icons.bookmark, size: 24)),
+                              child: Icon(Icons.bookmark_border_outlined,
+                                  size: 24)),
+                          const Align(
+                              alignment: Alignment.topLeft,
+                              child: Icon(Icons.trending_up, size: 24)),
                         ]),
                         const Align(
                           alignment: Alignment.centerLeft,
@@ -213,7 +201,203 @@ class _MyHomePageState extends State<MyHomePage> {
                               height: 90),
                           const Align(
                               alignment: Alignment.bottomRight,
-                              child: Icon(Icons.bookmark, size: 24)),
+                              child: Icon(Icons.bookmark_border_outlined,
+                                  size: 24)),
+                          const Align(
+                              alignment: Alignment.topLeft,
+                              child: Icon(Icons.trending_up, size: 24)),
+                        ]),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'TOM FORD BEAUTY(トムフォードビューティ)',
+                            style: TextStyle(
+                                fontSize: 11,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF777777)),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('アイ カラー クォード',
+                              style: TextStyle(fontSize: 11),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2),
+                        ),
+                        const SizedBox(height: 3),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('¥12,980', style: TextStyle(fontSize: 9)),
+                        ),
+                        const SizedBox(height: 3),
+                        Container(
+                          padding: const EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: const Color(0xFFF7F7F7)),
+                          child: const Row(
+                            children: [
+                              Icon(Icons.star, size: 9),
+                              Text('4.65', style: TextStyle(fontSize: 9)),
+                              Text('(8161件)', style: TextStyle(fontSize: 9)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 20),
+            const Align(alignment: Alignment.centerLeft, child: Text('ランキング')),
+            // ランキングアイテムのセルエリア
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // アイテムのセル
+                SizedBox(
+                  height: 210,
+                  width: 120,
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    margin: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white),
+                    child: Column(
+                      children: [
+                        Stack(children: [
+                          Image.network(
+                              'https://cloudflare.lipscosme.com/image/2a887d54a85339d23b882e29-1610080449.png',
+                              height: 90),
+                          const Align(
+                              alignment: Alignment.bottomRight,
+                              child: Icon(Icons.bookmark_border_outlined,
+                                  size: 24)),
+                        ]),
+                        const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'SUQQU',
+                              style: TextStyle(
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF777777)),
+                              overflow: TextOverflow.ellipsis,
+                            )),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('シグニチャー カラー アイズ',
+                              style: TextStyle(fontSize: 11),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2),
+                        ),
+                        const SizedBox(height: 3),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('¥7,700', style: TextStyle(fontSize: 9)),
+                        ),
+                        const SizedBox(height: 3),
+                        Container(
+                          padding: const EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: const Color(0xFFF7F7F7)),
+                          child: const Row(
+                            children: [
+                              Icon(Icons.star, size: 9),
+                              Text('4.62', style: TextStyle(fontSize: 9)),
+                              Text('(8726件)', style: TextStyle(fontSize: 9)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                // アイテムのセル
+                SizedBox(
+                  height: 210,
+                  width: 120,
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    margin: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white),
+                    child: Column(
+                      children: [
+                        Stack(children: [
+                          Image.network(
+                              'https://cloudflare.lipscosme.com/image/65b413ae555ebd623ae02de1-1699509212.png',
+                              height: 90),
+                          const Align(
+                              alignment: Alignment.bottomRight,
+                              child: Icon(Icons.bookmark_border_outlined,
+                                  size: 24)),
+                        ]),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Dior',
+                              style: TextStyle(
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF777777)),
+                              overflow: TextOverflow.ellipsis),
+                        ),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('ディオールショウ サンク クルール',
+                              style: TextStyle(fontSize: 11),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2),
+                        ),
+                        const SizedBox(height: 3),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('¥9,570', style: TextStyle(fontSize: 9)),
+                        ),
+                        const SizedBox(height: 3),
+                        Container(
+                          padding: const EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: const Color(0xFFF7F7F7)),
+                          child: const Row(
+                            children: [
+                              Icon(Icons.star, size: 9),
+                              Text('4.51', style: TextStyle(fontSize: 9)),
+                              Text('(913件)', style: TextStyle(fontSize: 9)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                // アイテムのセル
+                SizedBox(
+                  height: 210,
+                  width: 120,
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    margin: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white),
+                    child: Column(
+                      children: [
+                        Stack(children: [
+                          Image.network(
+                              'https://cloudflare.lipscosme.com/image/98a32d6a0f69e9873608d1de-1638145601.png',
+                              height: 90),
+                          const Align(
+                              alignment: Alignment.bottomRight,
+                              child: Icon(Icons.bookmark_border_outlined,
+                                  size: 24)),
                         ]),
                         const Align(
                           alignment: Alignment.centerLeft,
