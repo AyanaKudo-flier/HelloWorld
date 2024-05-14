660fcea1b2f48ffc1f139909ac52e3fc701dc3b6
Ayana Kudo <ayana.kudo@Kudos-MacBook-Pro.local>
2024-05-14 14:21:54 +0900

    feat: アイテムのウィジェットを追加、アプリ名とAppBarテキストを変更

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LIPSCloneApp',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hello, World!'),
+      home: const MyHomePage(title: 'ランキング'),
     );
   }
 }
@@ -370,6 +370,198 @@ class _MyHomePageState extends State<MyHomePage> {
                           const Align(
                             alignment: Alignment.centerLeft,
                             child: Text('ディオールショウ サンク クルール',
+                                style: TextStyle(fontSize: 11),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2),
+                          ),
+                          const SizedBox(height: 3),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child:
+                                Text('¥9,570', style: TextStyle(fontSize: 9)),
+                          ),
+                          const SizedBox(height: 3),
+                          Container(
+                            padding: const EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: const Color(0xFFF7F7F7)),
+                            child: const Row(
+                              children: [
+                                Icon(Icons.star, size: 9),
+                                Text('4.51', style: TextStyle(fontSize: 9)),
+                                Text('(913件)', style: TextStyle(fontSize: 9)),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  // アイテムのセル
+                  SizedBox(
+                    height: 210,
+                    width: 120,
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      margin: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                      child: Column(
+                        children: [
+                          Stack(children: [
+                            Image.network(
+                                'https://cloudflare.lipscosme.com/image/98a32d6a0f69e9873608d1de-1638145601.png',
+                                height: 90),
+                            const Align(
+                                alignment: Alignment.bottomRight,
+                                child: Icon(Icons.bookmark_border_outlined,
+                                    size: 24)),
+                          ]),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'TOM FORD BEAUTY(トムフォードビューティ)',
+                              style: TextStyle(
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF777777)),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text('アイ カラー クォード',
+                                style: TextStyle(fontSize: 11),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2),
+                          ),
+                          const SizedBox(height: 3),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child:
+                                Text('¥12,980', style: TextStyle(fontSize: 9)),
+                          ),
+                          const SizedBox(height: 3),
+                          Container(
+                            padding: const EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: const Color(0xFFF7F7F7)),
+                            child: const Row(
+                              children: [
+                                Icon(Icons.star, size: 9),
+                                Text('4.65', style: TextStyle(fontSize: 9)),
+                                Text('(8161件)', style: TextStyle(fontSize: 9)),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // アイテムのセル
+                  SizedBox(
+                    height: 210,
+                    width: 120,
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      margin: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                      child: Column(
+                        children: [
+                          Stack(children: [
+                            Image.network(
+                                'https://cloudflare.lipscosme.com/image/2a887d54a85339d23b882e29-1610080449.png',
+                                height: 90),
+                            const Align(
+                                alignment: Alignment.bottomRight,
+                                child: Icon(Icons.bookmark_border_outlined,
+                                    size: 24)),
+                          ]),
+                          const Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'SUQQU',
+                                style: TextStyle(
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF777777)),
+                                overflow: TextOverflow.ellipsis,
+                              )),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text('シグニチャー カラー アイズ',
+                                style: TextStyle(fontSize: 11),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2),
+                          ),
+                          const SizedBox(height: 3),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child:
+                                Text('¥7,700', style: TextStyle(fontSize: 9)),
+                          ),
+                          const SizedBox(height: 3),
+                          Container(
+                            padding: const EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: const Color(0xFFF7F7F7)),
+                            child: const Row(
+                              children: [
+                                Icon(Icons.star, size: 9),
+                                Text('4.62', style: TextStyle(fontSize: 9)),
+                                Text('(8726件)', style: TextStyle(fontSize: 9)),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  // アイテムのセル
+                  SizedBox(
+                    height: 210,
+                    width: 120,
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      margin: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                      child: Column(
+                        children: [
+                          Stack(children: [
+                            Image.network(
+                                'https://cloudflare.lipscosme.com/image/65b413ae555ebd623ae02de1-1699509212.png',
+                                height: 90),
+                            const Align(
+                                alignment: Alignment.bottomRight,
+                                child: Icon(Icons.bookmark_border_outlined,
+                                    size: 24)),
+                          ]),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text('Dior',
+                                style: TextStyle(
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF777777)),
+                                overflow: TextOverflow.ellipsis),
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            // issue: テキストを2行で表示しない場合も2行分のスペースを空けたい
+                            child: Text('ディオール',
                                 style: TextStyle(fontSize: 11),
                                 overflow: TextOverflow.ellipsis,
                                 maxLines: 2),
