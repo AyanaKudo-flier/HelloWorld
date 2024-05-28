1b97bb78af0d0c73e8b66922e455992229529dab
Ayana Kudo <ayana.kudo@Kudos-MacBook-Pro.local>
2024-05-28 15:39:41 +0900

    add: useFutureでuseMemoizedしてapi通信できるようにした

--- lib/ui/features/home/home_page.dart
+++ lib/ui/features/home/home_page.dart
@@ -10,28 +10,59 @@ import 'package:helloworld/data/entities/joke.dart';
 class HomePage extends HookConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  void getHttp() async {
-    try {
-      final jokeLocalState = useState<Joke>(const Joke(
-          id: 1, type: 'type', setup: 'setup', punchline: 'punchline'));
-      final Response response =
-          await Dio().get('https://official-joke-api.appspot.com/jokes/random');
-      final Joke joke = Joke.fromJson(response.data);
-      final Joke joke2 = joke.copyWith(id: 1);
-      jokeLocalState.value = joke2;
-      Logger().d(jokeLocalState.value);
-    } catch (e) {
-      Logger().e(e);
-    }
-  }
+  // void getHttp() async {
+  //   try {
+  //     final jokeLocalState = useState<Joke>(const Joke(
+  //         id: 1, type: 'type', setup: 'setup', punchline: 'punchline'));
+  //     final Response response =
+  //         await Dio().get('https://official-joke-api.appspot.com/jokes/random');
+  //     final Joke joke = Joke.fromJson(response.data);
+  //     final Joke joke2 = joke.copyWith(id: 1);
+  //     jokeLocalState.value = joke2;
+  //     Logger().d(jokeLocalState.value);
+  //   } catch (e) {
+  //     Logger().e(e);
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    getHttp();
+    final jokeLocalState = useState<Joke>(
+      const Joke(
+        id: 1,
+        type: 'type',
+        setup: 'setup',
+        punchline: 'punchline',
+      ),
+    );
+
+    final future = useFuture(
+      useMemoized(
+        () async {
+          try {
+            final Response response = await Dio()
+                .get('https://official-joke-api.appspot.com/jokes/random');
+            final Joke joke = Joke.fromJson(response.data);
+            final Joke joke2 = joke.copyWith(id: 1);
+            jokeLocalState.value = joke2;
+            Logger().d(jokeLocalState.value);
+          } catch (e) {
+            Logger().e(e);
+          }
+        },
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(title: const Text('ホーム')),
-      body: const Center(
-        child: Text('ホーム'),
+      body: Column(
+        children: [
+          if (future.hasError) const Text('エラーです'),
+          if (future.connectionState == ConnectionState.waiting)
+            const Text('データフェッチ中です'),
+          if (future.connectionState == ConnectionState.done)
+            Text(jokeLocalState.value.setup),
+        ],
       ),
     );
   }
