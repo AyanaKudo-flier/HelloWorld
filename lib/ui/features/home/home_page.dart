f10eb3cbf9708b6830deccbf2bcc6bb9b56d82d5
Ayana Kudo <ayana.kudo@Kudos-MacBook-Pro.local>
2024-05-28 15:43:54 +0900

    add: useFutureでuseMemoizedしてapi通信できるようにした

--- lib/ui/features/home/home_page.dart
+++ lib/ui/features/home/home_page.dart
@@ -8,22 +8,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:helloworld/data/entities/joke.dart';
 
 class HomePage extends HookConsumerWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  // void getHttp() async {
-  //   try {
-  //     final jokeLocalState = useState<Joke>(const Joke(
-  //         id: 1, type: 'type', setup: 'setup', punchline: 'punchline'));
-  //     final Response response =
-  //         await Dio().get('https://official-joke-api.appspot.com/jokes/random');
-  //     final Joke joke = Joke.fromJson(response.data);
-  //     final Joke joke2 = joke.copyWith(id: 1);
-  //     jokeLocalState.value = joke2;
-  //     Logger().d(jokeLocalState.value);
-  //   } catch (e) {
-  //     Logger().e(e);
-  //   }
-  // }
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
